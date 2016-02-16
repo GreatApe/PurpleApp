70674f4965d8a997d0a7182648362d5b9888d8ac
GreatApe <kugel@kth.se>
2016-02-16 01:24:22 +0100

    table set up in IB

---
+++ VeloApp/Models.swift
@@ -0,0 +1,134 @@
+//
+//  Models.swift
+//  VeloApp
+//
+//  Created by Gustaf Kugelberg on 11/02/16.
+//  Copyright © 2016 purple. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+// MARK: Dynamic models
+
+class TableX: Object {
+    dynamic var id = ""
+    dynamic var tableType: TableType?
+    
+//    dynamic var sourceTable: TableX?
+//    dynamic var tableFunction: Function?
+//    dynamic var elementFunction: Function?
+    
+    let elements = List<ElementX>()
+}
+
+class ElementX: Object {
+    let hiddenFieldIds = List<RealmString>()
+    
+    dynamic var d0: Double = 0.0
+    dynamic var d1: Double = 0.0
+//    dynamic var d2: Double = 0.0
+//    dynamic var d3: Double = 0.0
+    
+    dynamic var s0 = ""
+    dynamic var s1 = ""
+//    dynamic var s2 = ""
+//    dynamic var s3 = ""
+
+//    dynamic var t0 = NSDate.distantPast()
+//    dynamic var t1 = NSDate.distantPast()
+//    dynamic var t2 = NSDate.distantPast()
+//    dynamic var t3 = NSDate.distantPast()
+}
+
+// MARK: Fixed models
+
+class TableType: Object {
+    dynamic var id = ""
+    dynamic var elementTypeId = ""
+    
+    let computedColumns = List<ComputedColumn>()
+    let computedRows = List<ComputedRow>()
+}
+
+class ComputedColumn: Object {
+    dynamic var id = ""
+    dynamic var function: Function?
+}
+
+class ComputedRow: Object {
+    dynamic var id = ""
+    let computedRows = List<ComputedCell>()
+}
+
+class ComputedCell: Object {
+    dynamic var id = ""
+    dynamic var function: Function?
+}
+
+class Function: Object {
+    dynamic var id = ""
+    dynamic var elementTypeId = ""
+}
+
+class Canvas: Object {
+    dynamic var id = ""
+    let collections = List<Collection>()
+    let collectionLayouts = List<CollectionLayout>()
+    
+    let categories = List<Category>()
+    let mappings = List<Mapping>()
+}
+
+class Collection: Object {
+    dynamic var id = ""
+    let categoryValueIds = List<RealmString>()
+    
+    let categoryIds = List<RealmString>()
+    let groups = List<Group>()
+}
+
+class Group: Object {
+    dynamic var id = ""
+    
+    let tableIds = List<RealmString>()
+}
+
+class CollectionLayout: Object {
+    dynamic var collectionId = ""
+    
+    dynamic var leftCollection: CollectionLayout?
+    dynamic var leftDistance = 0
+    dynamic var topCollection: CollectionLayout?
+    dynamic var topDistance = 0
+    
+    let height = RealmOptional<Int>()
+    let width = RealmOptional<Int>()
+}
+
+class Category: Object {
+    dynamic var id = ""
+    let values = List<RealmString>()
+}
+
+class RealmString: Object {
+    dynamic var value = ""
+}
+
+class Mapping: Object {
+    dynamic var id = ""
+    let values = List<Correspondence>()
+}
+
+class Correspondence: Object {
+    dynamic var id = ""
+    dynamic var label = ""
+}
+
+// Meta
+// Elk, Elque
+// Moose
+// Boss
+// Money
+// Hype, Hypr
+
--- VeloApp/ViewController.swift
+++ VeloApp/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  VeloTableViewController.swift
 //  VeloApp
 //
 //  Created by Gustaf Kugelberg on 11/02/16.
@@ -7,19 +7,69 @@
 //
 
 import UIKit
+import RealmSwift
 
-class ViewController: UIViewController {
-
+class VeloTableViewController: UIViewController, UITableViewDelegate {
+    @IBOutlet weak var leftIndexColumnOffset: NSLayoutConstraint!
+    
+    @IBOutlet weak var leftIndexTableView: UITableView!
+    @IBOutlet weak var coreTableView: UITableView!
+    @IBOutlet weak var computedTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        leftIndexTableView.dataSource = IndexDataSource()
+        coreTableView.dataSource = RowDataSource()
+        computedTableView.dataSource = RowDataSource()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: UITableViewDelegate
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        leftIndexTableView.contentOffset = scrollView.contentOffset
+        coreTableView.contentOffset = scrollView.contentOffset
+        computedTableView.contentOffset = scrollView.contentOffset
     }
 
-
+    // MARK: User Actions
+    
+    @IBAction func tableNameChanged(sender: UITextField) {
+        sender.invalidateIntrinsicContentSize()
+    }
 }
+
+class IndexDataSource: NSObject, UITableViewDataSource {
+    let data = ["Donald", "Mickey", "Minney"]
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+class RowDataSource: NSObject, UITableViewDataSource {
+    let data = ["Donald", "Mickey", "Minney"]
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+
 
