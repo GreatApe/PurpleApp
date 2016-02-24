b9155dc4d2ee919cd454a73d6dee587a8f9df732
Andreas Okholm <aokholm@gmail.com>
2016-02-24 11:18:00 +0100

    Better sync, push table

--- VeloApp/FirebaseViewController.swift
+++ VeloApp/FirebaseViewController.swift
@@ -12,48 +12,86 @@ import Gloss
 
 class FirebaseViewController: UIViewController {
     
-    let ref = Firebase(url: "https://purplemist.firebaseio.com/")
-    var tables = [String : Table]()
+    var ds = DataSync()
     
     override func viewDidLoad() {
-        setupListeners()
+        ds.tableAdded = { (t: Table) in
+            print("add \(t.name)")
+        }
+        ds.tableChanged = { (t: Table) in
+            print("change \(t.name)")
+        }
+        ds.tableRemoved = { (t: Table) in
+            print("remove \(t.name)")
+        }
+    }
+}
+
+
+class DataSync {
+    let ref = Firebase(url: "https://purplemist.firebaseio.com/")
+    lazy var refTables: Firebase = { self.ref
+        .childByAppendingPath("users")
+        .childByAppendingPath(self.ref.authData.uid)
+        .childByAppendingPath("tables") }()
+    
+    var tableAdded: (Table -> Void)! { didSet { tableAdd() } }
+    var tableChanged: (Table -> Void)! { didSet { tableChange() } }
+    var tableRemoved: (Table -> Void)! { didSet { tableRemove() } }
+    
+    init() {
     }
     
+    func getPushId() -> String {
+        return ref.childByAutoId().key
+    }
     
-    func setupListeners() {
-        let refTables = ref
-            .childByAppendingPath("users")
-        .childByAppendingPath(ref.authData.uid)
-        .childByAppendingPath("tables")
-        
+    func setTable(t: Table) {
+        refTables.childByAppendingPath(t.key).setValue(t.rawdata)
+    }
+    
+    func setRow(key: String, rowindex: Int, r: [AnyObject]) {
+        refTables
+            .childByAppendingPath(key)
+            .childByAppendingPath(String(rowindex))
+            .setValue(r)
+    }
+    
+    private func tableAdd() {
         refTables.observeEventType(.ChildAdded) { (snap: FDataSnapshot!) -> Void in
-            guard let table = Table(object: snap.value) else {
+            guard let table = Table(key: snap.key, object: snap.value) else {
                 return
             }
-            self.tables[snap.key] = table
+            self.tableAdded(table)
             print("Table added: \(table)")
         }
-        
+    }
+    private func tableRemove() {
         refTables.observeEventType(.ChildRemoved) { (snap: FDataSnapshot!) -> Void in
-            if let table = self.tables[snap.key] {
-                print("Table removed: \(table)")
-            }
-            self.tables.removeValueForKey(snap.key)
-        }
-        
-        refTables.observeEventType(.ChildChanged) { (snap: FDataSnapshot!) -> Void in
-            guard let table = Table(object: snap.value) else {
+            guard let table = Table(key: snap.key, object: snap.value) else {
                 return
             }
-            self.tables[snap.key] = table
-            print("Table updated: \(table)")
+            self.tableRemoved(table)
+            print("Table removed: \(table)")
+        }
+    }
+    
+    private func tableChange() {
+        refTables.observeEventType(.ChildChanged) { (snap: FDataSnapshot!) -> Void in
+            guard let table = Table(key: snap.key, object: snap.value) else {
+                return
+            }
+            self.tableChanged(table)
+            print("Table changed: \(table)")
         }
     }
     
 }
 
+
 struct Table: CustomStringConvertible {
     let rawdata: [[AnyObject]]
+    let key: String
     
     var name: String {
         return toString(rawdata[0][0])
@@ -71,11 +109,19 @@ struct Table: CustomStringConvertible {
         return "\(name), header: \(headers), data: \(data)"
     }
     
-    init?(object: AnyObject!) {
+    init?(key: String, object: AnyObject!) {
+        self.key = key
         guard let d = object as? [[AnyObject]] where d.count > 2 else {
             return nil
         }
         rawdata = d
+    }
+    
+    init?(key: String, name: String, headers: [String], var data: [[AnyObject]]) {
+        self.key = key
+        data.insert([name, "", ""], atIndex: 0)
+        data.insert(headers, atIndex: 1)
+        rawdata = data
     }
     
     func toString(a: AnyObject) -> String {
